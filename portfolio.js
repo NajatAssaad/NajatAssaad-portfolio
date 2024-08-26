8922fec939a2accff640666b3ccffc9c020f89a5
NajatAssaad <najat.assaad02@gmail.com>
2024-08-27 00:38:58 +0300

    add some code for the burger menu and profile section

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,7 @@
 function toggleMenu(){
-    const menu = document.querySelector(".menu-links")
-    const icon = document.querySelector("hamburger-icon")
-    menu.classList.toggle(open);
-    icon.classList.toggle(open);
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open"); // iza ken 3nda lclass open bta3mlu remove w iza ma ken mawjud bta3mlu add
+    icon.classList.toggle("open");
 
 }
