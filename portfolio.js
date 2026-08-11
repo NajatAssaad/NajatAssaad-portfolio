48e99d8d9e7b9e2bbba691fbc78d42fdc786fef2
NajatAssaad <najat.assaad02@gmail.com>
2026-08-11 12:25:36 +1000

    portfolio update

--- portfolio.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open"); // iza ken 3nda lclass open bta3mlu remove w iza ma ken mawjud bta3mlu add
-    icon.classList.toggle("open");
-
-}
